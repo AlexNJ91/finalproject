6507085c4b41b54a6948934ff4b28d43fb3d1ac7
AlexNJ91 <alexnjohnston25@gmail.com>
2021-03-20 12:02:43 +0000

    larger application

--- testimonials/routes.py
+++ testimonials/routes.py
@@ -1,11 +1,12 @@
 from testimonials import app
+from flask import render_template
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'hello'
+    return render_template('index.html')
 
 
 @app.route('/api/testimonials')
 def testimonials():
-    return {'testimonial': ['great', 'ok']}
+    return {'testimonial': ["great", "ok"]}
