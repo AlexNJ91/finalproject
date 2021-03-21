7287b3e73234f205e9219548047cc98ece751f15
AlexNJ91 <alexnjohnston25@gmail.com>
2021-03-21 10:58:01 +0000

    larger application

--- testimonials/routes.py
+++ testimonials/routes.py
@@ -1,5 +1,5 @@
 from testimonials import app
-from flask import render_template
+from flask import render_template, abort
 
 testimonials = [
     {
@@ -20,13 +20,24 @@ testimonials = [
 ]
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', title="404 NOT FOUND"), 404
+
+
 @app.route('/')
 def index():
+    return render_template('index0.html')
+
+
+@app.route('/testimonials')
+def show_testimonials():
     return render_template('index.html', testimonials=testimonials)
 
 
-@app.route('/<id>')
+@app.route('/testimonials/<id>')
 def show_testimonial(id):
     for testimonial in testimonials:
         if testimonial.get('id') == int(id):
             return render_template('testimonial.html', testimonial=testimonial)
+    abort(404)
