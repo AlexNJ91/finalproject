31de3a0de86a1fe13c96037db845d6ef609a658d
AlexNJ91 <alexnjohnston25@gmail.com>
2021-03-24 12:34:14 +0000

    delete this

--- testimonials/routes.py
+++ testimonials/routes.py
@@ -1,23 +1,16 @@
 from testimonials import app, db, bcrypt, jwt
-from flask import render_template, abort, jsonify, request
+from flask import render_template, abort, jsonify, request, make_response
 from testimonials.models import Testimonial, User
 from flask_jwt_extended import jwt_required, create_access_token
+import datetime
 
 
-@app.route('/api/create_user', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    username = data.get('username')
-    password = bcrypt.generate_password_hash(data.get('password')).decode('utf-8')
-
-    user = User(username=username, password=password)
-    db.session.add(user)
-    db.session.commit()
-
-    return jsonify(user.id)
+@app.route('/')
+def index():
+    return render_template('index0.html')
 
 
-@app.route('/api/login', methods=['POST'])
+@app.route('/login')
 def login():
     data = request.get_json()
     username = data.get('username')
@@ -36,66 +29,34 @@ def login():
         return {'error': 'username or password incorrect'}, 400
 
 
-@app.route('/api/testimonials')
-def get_testimonials():
-    testimonials = Testimonial.query.all()
-    return jsonify({'testimonials': testimonials})
-
-
-@app.route('/api/testimonials/<id>')
-def get_testimonial(id):
-    testimonial = Testimonial.query.get(id)
-
-    if testimonial:
-        return jsonify(testimonial)
-
-    return{}
-
-
-@app.route('/api/testimonials', methods=['POST'])
-def add_testimonial():
+@app.route('/api/create_user', methods=['POST'])
+def create_user():
     data = request.get_json()
-    testimonial = Testimonial(name=data.get('name'), testimonial=data.get('testimonial'))
-    db.session.add(testimonial)
-    db.session.commit()
-    return jsonify(testimonial.id)
+    username = data.get('username')
+    password = bcrypt.generate_password_hash(data.get('password')).decode('utf-8')
 
-
-@app.route('/api/testimonials/<id>', methods=['PUT', 'POST'])
-def update_testimonial(id):
-    testimonial = Testimonial.query.get(id)
-
-    if not testimonial:
-        return {'error': 'not found'}, 400
-    data = request.get_json()
-    testimonial.name = data.get('name')
-    testimonial.testimonial = data.get('testimonial')
-    db.session.commit()
-    return jsonify(testimonial)
-
-
-@app.route('/api/testimonials/<id>', methods=['DELETE'])
-def delete_testimonial(id):
-    testimonial = Testimonial.query.get(id)
-
-    if not testimonial:
-        return {'error': 'not found'}, 400
-    db.session.delete(testimonial)
+    user = User(username=username, password=password)
+    db.session.add(user)
     db.session.commit()
 
-    return {}
+    return jsonify(user.id)
 
 
 testimonials = [
     {
         'id': 10,
-        'name': 'Connor',
-        'message': 'Your course helped me get a job'
+        'name': 'Conor',
+        'message': 'Alex has been great to work with. He was a pleasure to train in business development and an asset to the company'
     },
     {
         'id': 35,
         'name': 'Sarah',
-        'message': 'Great!'
+        'message': 'Alex was a great ambassador to the company and the ideas he brought to the table really drove us forward'
+    },
+    {
+        'id': 35,
+        'name': 'Joanne',
+        'message': 'We will be sad to see Alex leave. He has been a key member of our team and we wish him all the best'
     }
     ]
 
@@ -103,11 +64,6 @@ testimonials = [
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', title="404 NOT FOUND"), 404
-
-
-@app.route('/')
-def index():
-    return render_template('index0.html')
 
 
 @app.route('/testimonials')
